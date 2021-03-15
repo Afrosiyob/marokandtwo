a9ac3e604859b6581ed921eafc78b64f2516ab3f
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-03-15 21:59:59 +0500

    ad
    add

--- js/index.js
+++ js/index.js
@@ -1,20 +1,9 @@
 $(document).ready(function() {
 
-
-
-
-
-
-    // image box
-
-
-
-
+    // Quil Text Editor
 
     var quill = new Quill('#editor', {
-
         placeholder: 'Compose an epic...',
-
         theme: 'snow',
     });
 
@@ -24,29 +13,29 @@ $(document).ready(function() {
         if (input.files) {
             var filesAmount = input.files.length;
 
+
+            for (const index in input.files) {
+                if (Object.hasOwnProperty.call(input.files, index)) {
+                    const element = input.files[index];
+                    console.log(element);
+
+                }
+            }
+
             for (i = 0; i < filesAmount; i++) {
                 var reader = new FileReader();
-
                 reader.onload = function(event) {
                     $($.parseHTML('<img>')).attr({
                         'src': event.target.result,
-                        // 'onclick': img_box(event.target.result)
                     }).appendTo(placeToInsertImagePreview);
                 }
-
                 reader.readAsDataURL(input.files[i]);
             }
         }
-
     };
 
     $('#gallery-photo-add').on('change', function() {
         imagesPreview(this, 'div.gallery');
     });
-
-
-
-
-
 
 });
