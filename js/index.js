57d5538639ce36b59090ae30f25efd10ac726967
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-03-16 18:08:32 +0500

    add
    add

--- js/index.js
+++ js/index.js
@@ -1,41 +1,126 @@
-$(document).ready(function() {
+$( document ).ready( function () {
 
     // Quil Text Editor
 
-    var quill = new Quill('#editor', {
+    var quill = new Quill( '#editor', {
         placeholder: 'Compose an epic...',
         theme: 'snow',
-    });
+    } );
 
     // Multiple images preview in browser
-    var imagesPreview = function(input, placeToInsertImagePreview) {
+    var imagesPreview = function ( input, placeToInsertImagePreview ) {
 
-        if (input.files) {
+        if ( input.files ) {
             var filesAmount = input.files.length;
 
 
-            for (const index in input.files) {
-                if (Object.hasOwnProperty.call(input.files, index)) {
-                    const element = input.files[index];
-                    console.log(element);
+            for ( const index in input.files ) {
+                if ( Object.hasOwnProperty.call( input.files, index ) ) {
+                    const element = input.files[ index ];
+                    console.log( element );
 
                 }
             }
 
-            for (i = 0; i < filesAmount; i++) {
+            for ( i = 0; i < filesAmount; i++ ) {
                 var reader = new FileReader();
-                reader.onload = function(event) {
-                    $($.parseHTML('<img>')).attr({
+                reader.onload = function ( event ) {
+                    $( $.parseHTML( '<img>' ) ).attr( {
                         'src': event.target.result,
-                    }).appendTo(placeToInsertImagePreview);
+                    } ).appendTo( placeToInsertImagePreview );
                 }
-                reader.readAsDataURL(input.files[i]);
+                reader.readAsDataURL( input.files[ i ] );
             }
         }
     };
 
-    $('#gallery-photo-add').on('change', function() {
-        imagesPreview(this, 'div.gallery');
-    });
+    $( '#gallery-photo-add' ).on( 'change', function () {
+        imagesPreview( this, 'div.gallery' );
+    } );
 
-});
+
+
+
+
+
+    // Drag and Drop
+
+
+    const dragAndDrop = () => {
+
+
+        const cards = document.querySelectorAll( '.drag_body_inner_item' );
+        const columns = document.querySelectorAll( '.drag_column_body' );
+        var card_item;
+
+        const dragStart = function () {
+
+            card_item = this;
+
+            setTimeout( () => {
+                this.classList.add( 'hide_card' );
+            }, 0 );
+        }
+
+
+
+
+        const dragEnd = function () {
+            card_item = null;
+            setTimeout( () => {
+                this.classList.remove( 'hide_card' );
+            }, 0 );
+        }
+
+        cards.forEach( card => {
+            card.addEventListener( "dragstart", dragStart );
+            card.addEventListener( 'dragend', dragEnd );
+        } );
+
+
+
+
+        const dragOver = function ( e ) {
+            e.preventDefault();
+            console.log( "over" );
+        }
+
+        const dragEnter = function () {
+            console.log( "enter" );
+
+            // this.parentElement.classList.add( 'hovered_card' );
+        }
+
+        const dragLeave = function () {
+            console.log( "leave" );
+
+            // this.parentElement.classList.remove( 'hovered_card' );
+        }
+
+        const dragDrop = function ( e ) {
+            console.log( e );
+
+            this.append( card_item );
+        }
+
+        columns.forEach( column => {
+
+
+            column.addEventListener( 'dragover', dragOver );
+            column.addEventListener( 'dragenter', dragEnter );
+            column.addEventListener( 'dragleave', dragLeave );
+            column.addEventListener( 'drop', dragDrop );
+        } );
+
+
+
+
+
+
+    }
+
+
+
+    dragAndDrop();
+
+} );
